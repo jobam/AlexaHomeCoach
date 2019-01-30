aff5647b8e3c9e51bbe5e6b9d70e066498208351
Jonathan Abraham <jonathan.abraham@outlook.com>
2019-01-30 11:16:42 +0100

    feat: migrate Function to API

---
+++ HomeCoach.Api/Controllers/TemperatureController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeCoach.Api.Controllers
+{
+    using Alexa.NET;
+    using Alexa.NET.Request;
+    using Alexa.NET.Request.Type;
+    using Alexa.NET.Response;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemperatureController : ControllerBase
+    {
+        [HttpPost]
+        [Route("temperature")]
+        public IActionResult GetTemperature(SkillRequest skillRequest)
+        {          
+            var requestType = skillRequest.GetRequestType();
+
+            SkillResponse response = null;
+
+            if (requestType == typeof(LaunchRequest))
+            {
+                response = ResponseBuilder.Tell("Welcome to HomeCoach!");
+                response.Response.ShouldEndSession = false;
+            }
+
+            return this.Ok(response);
+        }
+        }
+
+       
+    }
+}
