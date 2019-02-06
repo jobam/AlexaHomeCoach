193c19cf83eed2f5667918c01e34c84f355e86b6
Jonathan Abraham <jonathan.abraham@outlook.com>
2019-02-06 09:19:27 +0100

    feat: Add temperature model
    Related Work Items:

---
+++ HomeCoach.Business/Models/HomeCoachData.cs
@@ -0,0 +1,14 @@
+namespace HomeCoach.Business.Models
+{
+    public class HomeCoachData
+    {
+        public string DeviceName { get; set; }
+
+        public double Temperature { get; set; }
+        
+        public double Humidity { get; set; }
+        
+        public double Noise { get; set; }
+        
+    }
+}
